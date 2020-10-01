80808cffa5cc48e742e61a9a54000a994eac35e9
Ema <puscasemanuel7@gmail.com>
2020-10-01 19:05:17 +0300

    server files

---
+++ back-end/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const products = require('./data/products');
+
+dotenv.config();
+
+const app = express();
+
+app.get('/api/products', (req, res) => {
+  res.json(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((el) => el._id === req.params.id);
+  res.json(product);
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(
+  port,
+  console.log(`Server running in ${process.env.NODE_ENV} on port ${port}`)
+);
