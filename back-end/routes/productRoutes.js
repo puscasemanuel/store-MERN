37c2da262e6def9bbd72cae1a320369933cf4db5
Ema <puscasemanuel7@gmail.com>
2020-10-01 23:16:19 +0300

    Product routes

---
+++ back-end/routes/productRoutes.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const asyncHandler = require('express-async-handler');
+const router = express.Router();
+const Product = require('../models/productModel');
+
+//Get all products
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+  })
+);
+
+//Get product by id
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404);
+      throw new Error('Product not found');
+    }
+  })
+);
+
+module.exports = router;
