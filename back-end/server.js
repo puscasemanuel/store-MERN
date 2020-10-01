2d0c30e28a1ac85c1c04a189608412de987d49d5
Ema <puscasemanuel7@gmail.com>
2020-10-01 22:14:29 +0300

    db connection and products data

--- back-end/server.js
+++ back-end/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const db = require('./config/db');
+const colors = require('colors');
 const products = require('./data/products');
 
 dotenv.config();
+
+db.connectDB();
 
 const app = express();
 
@@ -19,5 +23,7 @@ const port = process.env.PORT || 5000;
 
 app.listen(
   port,
-  console.log(`Server running in ${process.env.NODE_ENV} on port ${port}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} on port ${port}`.yellow.bold
+  )
 );
